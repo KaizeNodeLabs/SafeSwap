ac455768a92bad90267973241c9da38875b40ca6
JoelVR17 <vermudo.com@gmail.com>
2024-12-17 19:30:39 -0600

    feat: Remove unnecessary fragment in Bounded component for cleaner markup

--- frontend/app/components/Bounded.tsx
+++ frontend/app/components/Bounded.tsx
@@ -5,12 +5,10 @@ interface BoundedProps {
 
 const Bounded = ({ children, title }: BoundedProps) => {
   return (
-    <>
-      <main className="p-8">
-        <h1 className="text-3xl font-bold mb-8">{title}</h1>
-        <div className="flex flex-wrap justify-center gap-8">{children}</div>
-      </main>
-    </>
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-8">{title}</h1>
+      <div className="flex flex-wrap justify-center gap-8">{children}</div>
+    </main>
   );
 };
 
