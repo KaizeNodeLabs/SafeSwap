b75e630fe076bfb44c7de1b532e6b5cd6c5d1e5a
JoelVR17 <vermudo.com@gmail.com>
2024-12-17 19:30:57 -0600

    feat: Enhance ProductDetails page with SubHeader and Images components for improved layout and functionality

--- frontend/app/products/[productId]/page.tsx
+++ frontend/app/products/[productId]/page.tsx
@@ -1,6 +1,10 @@
 import Bounded from "@/app/components/Bounded";
+import SubHeader from "@/app/components/header/subheader/SubHeader";
+import Images from "@/app/components/products/Images";
 import { products } from "@/constants/testDataProduct";
 import { Product } from "@/entities/Product";
+import { Button } from "@radix-ui/themes";
+import { Share2, ShoppingCart } from "lucide-react";
 
 interface ProductDetailsProps {
   params: {
@@ -17,11 +21,60 @@ const ProductDetails = ({ params }: ProductDetailsProps) => {
 
   return (
     <>
+      <SubHeader name="Product" />
       <Bounded title="Product Details">
-        <h1>{product?.name}</h1>
-        <p>{product?.category}</p>
-        <p>{product?.description}</p>
-        <p>{product?.price}</p>
+        <div className="flex justify-center w-2/3 gap-10 mx-auto py-10">
+          {product && (
+            <section className="w-1/2">
+              <Images images={product.images} />
+            </section>
+          )}
+
+          <section className="w-1/2 space-y-6">
+            <div>
+              <h1 className="text-3xl font-bold">{product?.name}</h1>
+              <p className="text-zinc-500 text-xl font-semibold mt-3">$98.00</p>
+            </div>
+
+            <div>
+              <h3 className="font-medium mb-2">Category:</h3>
+              <p className="text-gray-700 text-sm leading-relaxed">
+                {product?.category}
+              </p>
+            </div>
+
+            <div>
+              <h3 className="font-medium mb-2">Quantity:</h3>
+              <div className="flex items-center gap-2">
+                <button className="border px-3 py-1 rounded hover:bg-gray-100">
+                  -
+                </button>
+                <span>1</span>
+                <button className="border px-3 py-1 rounded hover:bg-gray-100">
+                  +
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <h3 className="font-medium mb-2">Description:</h3>
+              <p className="text-gray-700 text-sm leading-relaxed">
+                {product?.description}
+              </p>
+            </div>
+
+            <div className="flex justify-center items-center gap-3">
+              <Button className="bg-black text-white w-9/12 cursor-pointer">
+                <ShoppingCart className="mr-2 h-4 w-4" />
+                Add to Cart
+              </Button>
+              <Button className="bg-black text-white cursor-pointer">
+                <Share2 className="mr-2 h-4 w-4" />
+                Share
+              </Button>
+            </div>
+          </section>
+        </div>
       </Bounded>
     </>
   );
