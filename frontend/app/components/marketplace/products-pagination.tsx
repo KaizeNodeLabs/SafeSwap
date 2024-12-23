87727a73046a62dd9bf7bd784f60a9d37e7b466c
Derian <dmrodriguez2000@gmail.com>
2024-12-22 20:52:15 -0600

    style: update pagination styles

--- frontend/app/components/marketplace/products-pagination.tsx
+++ frontend/app/components/marketplace/products-pagination.tsx
@@ -19,7 +19,7 @@ import {
 export const ProductsPagination = () => {
 	return (
 		<section className="w-full pt-5 mt-2 flex flex-col gap-5 md:flex-row md:gap-0">
-			<div className="w-full gap-2 flex items-center md:w-[50%]">
+			<div className="w-full gap-2 flex items-center justify-center md:justify-start md:w-[50%]">
 				<label className="opacity-80">Show result:</label>
 				<Select>
 					<SelectTrigger className="w-[70px]">
@@ -38,7 +38,7 @@ export const ProductsPagination = () => {
 			</div>
 
 			<div className="w-full md:w-[50%]">
-				<Pagination className="justify-end">
+				<Pagination className="justify-center md:justify-end">
 					<PaginationContent>
 						<PaginationItem>
 							<PaginationPrevious href="#" />
