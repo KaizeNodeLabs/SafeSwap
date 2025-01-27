5e798dac9dd7477305d0e3a6e596545db34097d3
Erick Vasquez <123427413+evgongora@users.noreply.github.com>
2025-01-27 13:16:41 -0600

    fix: requested changes

--- apps/frontend/app/components/shared/header/shopping-cart-modal.tsx
+++ apps/frontend/app/components/shared/header/shopping-cart-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Minus, Plus, ShoppingBag, Trash2, X } from "lucide-react";
+import { Minus, Plus, ShoppingBag, Trash2 } from "lucide-react";
 import Image from "next/image";
 
 import { Button } from "@/app/components/ui/button";
