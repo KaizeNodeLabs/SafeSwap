961de776aafb1655bf36c0e8ff7aee2bf8dd6e33
Shukazuby <shukazuby@gmail.com>
2025-02-11 10:50:09 +0100

    Added product Image dto

--- apps/backend/src/modules/product-image/dto/productimage.input.ts
+++ apps/backend/src/modules/product-image/dto/productimage.input.ts
@@ -2,5 +2,9 @@ import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class ProductImageDTO {
+    @Field(() => String)
+    imageUrl: string;
 
+    @Field(() => String)
+    productId: string;
 }
