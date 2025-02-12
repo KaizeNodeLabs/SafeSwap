7bd904ccce96cbe3ba8f27d2511f719b8abd3df8
Derian <dmrodriguez2000@gmail.com>
2025-02-11 19:03:25 -0600

    refactor: simplify filters logic

--- apps/frontend/components/marketplace/products-not-found.tsx
+++ apps/frontend/components/marketplace/products-not-found.tsx
@@ -1,38 +1,27 @@
 "use client";
 
-import { useTranslations } from "@/hooks/useTranslations";
 import { PackageSearch } from "lucide-react";
-import React, { type Dispatch, type SetStateAction } from "react";
-import { Button } from "../ui/button";
+
+import { useTranslations } from "@/hooks/useTranslations";
 import NotFound from "../shared/not-found";
+import { Button } from "../ui/button";
 
-type ProductsNotFoundProps = {
-  setPriceRange: Dispatch<SetStateAction<[number, number]>>;
-  setSelectedCategories: Dispatch<SetStateAction<string[]>>;
-};
+interface ProductsNotFoundProps {
+	onClear: () => void;
+}
 
-export default function ProductsNotFound({
-  setPriceRange,
-  setSelectedCategories,
-}: ProductsNotFoundProps) {
-  const { t } = useTranslations();
+export default function ProductsNotFound({ onClear }: ProductsNotFoundProps) {
+	const { t } = useTranslations();
 
-  const clearFilters = () => {
-    setPriceRange([0, 1500]);
-    setSelectedCategories([]);
-  };
+	return (
+		<section className="flex flex-col items-center justify-center space-y-4 mx-auto">
+			<NotFound
+				icon={PackageSearch}
+				title={t("common.noProducts.title")}
+				description={t("common.noProducts.description")}
+			/>
 
-  return (
-    <section className="flex flex-col items-center justify-center space-y-4 mx-auto">
-      <NotFound
-        icon={PackageSearch}
-        title={t("common.noProducts.title")}
-        description={t("common.noProducts.description")}
-      />
-
-      <Button onClick={clearFilters}>
-        {t("common.noProducts.clearFilters")}
-      </Button>
-    </section>
-  );
+			<Button onClick={onClear}>{t("common.noProducts.clearFilters")}</Button>
+		</section>
+	);
 }
