7f9a1b915254a785235cda372945c45640599fb0
gadyrcdz <gadyrcalde@gmail.com>
2025-02-23 15:56:36 -0600

    feat: Created the new component product-carousel and test-implemented in shopping page

--- apps/frontend/app/(routes)/shopping/page.tsx
+++ apps/frontend/app/(routes)/shopping/page.tsx
@@ -1,3 +1,15 @@
+import ProductCarousel from "@/components/home/product-carousel";
+
 export default function ShoppingPage() {
-	return <div>ShoppingPage</div>;
+	return (
+		<div>
+			<h1 className="text-3xl font-bold mb-4">Featured Products </h1>
+			<ProductCarousel />
+		</div>
+	);
 }
+
+//Old content of Shopping Page
+// export default function ShoppingPage() {
+// 	return <div>ShoppingPage</div>;
+// }
