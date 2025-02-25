536a4395b73b931f35b9cd687325f2a871fbbab3
Daniel Villatoro Cantarero <danielvillatoroc@gmail.com>
2025-02-24 18:59:54 -0600

    feat: #161 Add new testimonial route

---
+++ apps/frontend/app/(routes)/testimonial/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import TestimonialsCarousel from "@/components/testimonials/TestimonialsCarousel";
+
+export default function TestimonialPage() {
+	return <TestimonialsCarousel />;
+}
