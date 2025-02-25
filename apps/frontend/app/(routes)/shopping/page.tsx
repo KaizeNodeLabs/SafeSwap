61fb60cc92d9b7fc72d3d035c5048afc6f238fbb
gadyrcdz <gadyrcalde@gmail.com>
2025-02-25 09:11:08 -0600

    feat: Deleted test code in shopping page

--- apps/frontend/app/(routes)/shopping/page.tsx
+++ apps/frontend/app/(routes)/shopping/page.tsx
@@ -1,17 +1,3 @@
-import ProductCarousel from "@/components/home/product-carousel";
-
 export default function ShoppingPage() {
-	return (
-		<div className="container mx-auto p-4 sm:p-6 lg:p-8">
-			<h1 className="text-3xl font-bold mb-4 text-center sm:text-left">
-				ShoppingPage
-			</h1>
-			<ProductCarousel />
-		</div>
-	);
+	return <div>ShoppingPage</div>;
 }
-
-//Old content of Shopping Page
-// export default function ShoppingPage() {
-// 	return <div>ShoppingPage</div>;
-// }
